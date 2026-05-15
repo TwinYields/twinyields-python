a8ae7266d17764706fa467fac2138c631f1e70fd
Matti Pastell <430863+mpastell@users.noreply.github.com>
2026-05-15 09:27:14 +0300

    Fixes to NasaPower updater

--- twinyields/database/nasapower.py
+++ twinyields/database/nasapower.py
@@ -13,8 +13,9 @@ class NasaPowerUpdater(object):
     def get_met_dataframe(self, latitude, longitude, start, end):
         url = "https://power.larc.nasa.gov/api/temporal/daily/point?parameters="
         # From apsimx R package
-        params = "T2M_MAX,T2M_MIN,ALLSKY_SFC_SW_DWN,PRECTOTCORR,RH2M,WS2M"
-        query = "&community=RE&longitude={longitude}&latitude={latitude}&start={start}&end={end}&format=JSON&time-standard=LST"
+        params = "T2M,T2M_MAX,T2M_MIN,ALLSKY_SFC_SW_DWN,PRECTOTCORR,RH2M,WS2M,EVPTRNS,GWETROOT,GWETTOP"
+
+        query = "&community=AG&longitude={longitude}&latitude={latitude}&start={start}&end={end}&format=JSON&time-standard=LST"
         api_request_url = url + params + query.format(longitude=longitude, latitude=latitude, start=start,
                                                            end=end)
         response = requests.get(url=api_request_url, verify=True, timeout=30.00)
@@ -24,10 +25,12 @@ class NasaPowerUpdater(object):
         data = content["properties"]["parameter"]
         df = pd.DataFrame.from_dict(data, orient="columns")
         df["date"] = [pd.Timestamp(x) for x in df.index]
-        df["year"] = df["date"].dt.year
+        #df["year"] = df["date"].dt.year
         df["day"] = df["date"].dt.dayofyear
-        df = df[["year", "day", "ALLSKY_SFC_SW_DWN", "T2M_MAX", "T2M_MIN", "PRECTOTCORR", "RH2M", "WS2M"]]
-        df.columns = ["year", "day", "radn", "maxt", "mint", "rain", "rh", "windspeed"]
+        df = df[["date", "day", "ALLSKY_SFC_SW_DWN", "T2M", "T2M_MAX", "T2M_MIN", 
+                 "PRECTOTCORR", "RH2M", "WS2M", "EVPTRNS", "GWETROOT", "GWETTOP"]]
+        # GWE = soil wetness
+        df.columns = ["date", "day", "radn", "meant", "maxt", "mint", "rain", "rh", "windspeed", "ET0", "swroot", "swtop"]
         df["latitude"] = latitude
         df["longitude"] = longitude
         return df
@@ -39,15 +42,14 @@ class NasaPowerUpdater(object):
         df = self.get_met_dataframe(latitude, longitude, start, end)
         # daily means to use as forecast
         df_mean = df.groupby(["day"], as_index=False).mean()
-        del df_mean["year"]
         self.db.save_dataframe(df, "NasaPowerDaily")
         self.db.save_dataframe(df_mean, "NasaPowerDailyMean")
 
     def update_history(self, years=20):
         s = datetime.datetime.now().year - 1 - years
         e = datetime.datetime.now().year - 1
-        f,z = self.db.get_field()
-        self.update_met_db(f["latitude"].round(3)[0], f["longitude"].round(3)[0], f"{s}0101", f"{e}1231")
+        lon, lat = self.db.read_geo_dataframe("Parcels").union_all().centroid.xy
+        self.update_met_db(round(lat[0], 3), round(lon[0], 3), f"{s}0101", f"{e}1231")
 
 
 
